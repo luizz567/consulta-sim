31417cb51d849db072515f7a4f69e7209a988828
luiz567 <martins.567@hotmail.com>
2022-08-28 18:15:19 -0300

    testes do model de paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'Salvar paciente com o nome invalido' do
+    paciente = Paciente.new nome: 'Luiz Gus1tavo', data_nascimento: '19-04-1999', cpf: '12692003489', email: 'martins.567@hotmail.com'
+    assert_not paciente.save
+  end
+
+  test 'Salvar paciente com dados corretos' do
+    paciente = Paciente.new nome: 'Genaro Leite', data_nascimento: '24-09-1967', cpf: '12692173489', email: 'genaro.567@hotmai.com'
+    assert paciente.save
+  end
+
 end
